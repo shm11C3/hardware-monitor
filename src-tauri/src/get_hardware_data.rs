520b08dc622c0a2c1079362aa830bae88d30b568
shm <m11c3.sh@gmail.com>
2024-06-03 01:52:35 +0900

    refactor: format file

--- src-tauri/src/get_hardware_data.rs
+++ src-tauri/src/get_hardware_data.rs
@@ -7,23 +7,23 @@ use sysinfo::System;
 use tauri::command;
 
 pub struct AppState {
-   pub system: Arc<Mutex<System>>,
+  pub system: Arc<Mutex<System>>,
 }
 
 #[command]
 pub fn get_cpu(state: tauri::State<'_, AppState>) -> f32 {
-   let system = state.system.lock().unwrap();
-   let cpus = system.cpus();
-   let total_usage: f32 = cpus.iter().map(|cpu| cpu.cpu_usage()).sum();
-   total_usage / cpus.len() as f32
+  let system = state.system.lock().unwrap();
+  let cpus = system.cpus();
+  let total_usage: f32 = cpus.iter().map(|cpu| cpu.cpu_usage()).sum();
+  total_usage / cpus.len() as f32
 }
 
 pub fn initialize_system(system: Arc<Mutex<System>>) {
-   thread::spawn(move || loop {
-      {
-         let mut sys = system.lock().unwrap();
-         sys.refresh_cpu();
-      }
-      thread::sleep(Duration::from_secs(1));
-   });
+  thread::spawn(move || loop {
+    {
+      let mut sys = system.lock().unwrap();
+      sys.refresh_cpu();
+    }
+    thread::sleep(Duration::from_secs(1));
+  });
 }
