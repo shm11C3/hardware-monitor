1dd031dc06f9c46731c42db6845c6ea52e6dc807
shm <m11c3.sh@gmail.com>
2024-06-02 23:59:42 +0900

    add CPU使用率取得検証用のコード追加

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,24 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+
+mod get_hardware_data;
+
+use get_hardware_data::{initialize_system, AppState, get_cpu};
+use std::sync::{Arc, Mutex};
+use sysinfo::System;
+
 fn main() {
+  let system = Arc::new(Mutex::new(System::new_all()));
+  let state = AppState {
+      system: Arc::clone(&system),
+  };
+
+  initialize_system(system);
+
   tauri::Builder::default()
+    .manage(state)
+    .invoke_handler(tauri::generate_handler![get_cpu])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
---
+++ src/lib/components/sample.svelte
@@ -0,0 +1,17 @@
+<script>
+import { invoke } from "@tauri-apps/api/tauri";
+
+// biome-ignore lint/style/useConst: <explanation>
+let name = "";
+let greetMsg = "";
+
+async function getCpu() {
+	greetMsg = await invoke("get_cpu", { name });
+}
+</script>
+
+<div>
+  <input id="greet-input" placeholder="Enter a name..." bind:value="{name}" />
+  <button on:click="{getCpu}">Greet</button>
+  <p>{greetMsg}</p>
+</div>
