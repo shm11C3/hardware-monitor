f82109ca9bda2283e82c108c799045eedb9830b9
shm <m11c3.sh@gmail.com>
2024-06-08 23:59:06 +0900

    refactor: 不要なコードを削除

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
-import Greet from "../lib/components/sample.svelte";
+import TestTemplate from "../lib/components/sample.svelte";
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<Greet />
+
+<TestTemplate />
