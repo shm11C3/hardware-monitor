9eb41e19326f1383ba3716cc260f6bd71c141d50
shm <m11c3.sh@gmail.com>
2024-09-15 19:47:50 +0900

    refactor: 不要なコードの削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import TestTemplate from "./components/Sample";
 import ChartTemplate from "./template/Chart";
 import "./index.css";
 import {
@@ -11,10 +10,7 @@ import { useHardwareInfoAtom } from "./atom/useHardwareInfoAtom";
 import { useSettingsAtom } from "./atom/useSettingsAtom";
 import { useDarkMode } from "./hooks/useDarkMode";
 
-type ButtonState = "chart" | "raw";
-
 const Page = () => {
-  const [buttonState, setButtonState] = useState<ButtonState>("chart");
   const { settings } = useSettingsAtom();
   const { toggle } = useDarkMode();
 
@@ -22,10 +18,6 @@ const Page = () => {
   useErrorModalListener();
   const { hardwareInfo } = useHardwareInfoAtom();
   console.log(hardwareInfo);
-
-  const handleShowData = () => {
-    setButtonState(buttonState === "raw" ? "chart" : "raw");
-  };
 
   useEffect(() => {
     if (settings?.theme) {
@@ -36,12 +28,7 @@ const Page = () => {
   return (
     <div className="bg-slate-200 dark:bg-gray-900 text-gray-900 dark:text-white">
       <h1>Hardware Monitor Proto</h1>
-      <div>
-        <button type="button" onClick={handleShowData}>
-          {buttonState === "chart" ? "Show Raw Data" : "Show Chart Sample"}
-        </button>
-      </div>
-      {buttonState === "raw" ? <TestTemplate /> : <ChartTemplate />}
+      <ChartTemplate />
       <SettingsSheet />
     </div>
   );
